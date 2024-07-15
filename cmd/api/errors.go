595bbb17891a1d426806cb02b7ef504fd39a2403
Atila Guler <atilagulerdev@gmail.com>
2024-07-16 01:53:06 +0300

    add recover panic

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -28,21 +28,15 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	app.logError(r, err)
 
 	message := "the server encountered a problem and could not process your request"
-
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
-func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logError(r, err)
-
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request, err error) {
-	app.logError(r, err)
-
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
-
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
